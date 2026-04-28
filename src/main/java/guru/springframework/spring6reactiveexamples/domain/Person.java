f5856d62677ea85be6c8f14be47e1d6ca4aa4e80
Jandier Rojas Zúñiga <jandier.rojas.z@gmail.com>
2026-04-28 10:13:01 -0600

    Adding Person, PersonRepository and PersonRepositoryImpl

---
+++ src/main/java/guru/springframework/spring6reactiveexamples/domain/Person.java
@@ -0,0 +1,18 @@
+package guru.springframework.spring6reactiveexamples.domain;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+public class Person {
+
+    private Integer id;
+    private String firstName;
+    private String lastName;
+}
