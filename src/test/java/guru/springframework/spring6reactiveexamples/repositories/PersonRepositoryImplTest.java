0ff9da3e7cc04f9bb6ff7510562b12a91bf82cfd
Jandier Rojas Zúñiga <jandier.rojas.z@gmail.com>
2026-04-28 10:23:53 -0600

    Some tests

---
+++ src/test/java/guru/springframework/spring6reactiveexamples/repositories/PersonRepositoryImplTest.java
@@ -0,0 +1,36 @@
+package guru.springframework.spring6reactiveexamples.repositories;
+
+import guru.springframework.spring6reactiveexamples.domain.Person;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Mono;
+
+class PersonRepositoryImplTest {
+
+    PersonRepository personRepository = new PersonRepositoryImpl();
+
+    @Test
+    void testMonoByIdBlock() {
+        Mono<Person> personMono = personRepository.getById(1);
+
+        Person person = personMono.block();
+
+        System.out.println(person.toString());
+    }
+
+
+    @Test
+    void testGetByIdSubscriber() {
+        Mono<Person> personMono = personRepository.getById(1);
+
+        personMono.subscribe(person -> System.out.println(person.toString()));
+    }
+
+    @Test
+    void testMapOperation() {
+
+        Mono<Person> personMono = personRepository.getById(1);
+
+        personMono.map(person -> person.getLastName())
+                .subscribe(firstName -> System.out.println(firstName));
+    }
+}
